ab57e91cb9200be28c90b86f0102118df2808844
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-04-20 22:48:31 -0700

    keypress fn added

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-var buttons = document.querySelectorAll(".drum");
-for (let i = 0; i < buttons.length; i++) {
+// BUTTON PRESS DETECTION
+
+var buttonNum = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttonNum; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        var audio = new Audio("./sounds/crash.mp3");
-        audio.play();
-        break;
-      case "a":
-        new Audio("./sounds/kick-bass.mp3").play();
-        break;
-      case "s":
-        new Audio("./sounds/snare.mp3").play();
-        break;
-      case "d":
-        new Audio("./sounds/tom-1.mp3").play();
-        break;
-      case "j":
-        new Audio("./sounds/tom-2.mp3").play();
-        break;
-      case "k":
-        new Audio("./sounds/tom-3.mp3").play();
-        break;
-      case "l":
-        new Audio("./sounds/tom-4.mp3").play();
-        break;
-
-      default:
-    }
+    soundMaker(buttonInnerHTML);
   });
 }
 
+//KEYBOARD PRESS DETECTION
 
+document.addEventListener("keydown", function (event) {
+  soundMaker(event.key);
+});
+function soundMaker(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      new Audio("./sounds/kick-bass.mp3").play();
+      break;
+    case "s":
+      new Audio("./sounds/snare.mp3").play();
+      break;
+    case "d":
+      new Audio("./sounds/tom-1.mp3").play();
+      break;
+    case "j":
+      new Audio("./sounds/tom-2.mp3").play();
+      break;
+    case "k":
+      new Audio("./sounds/tom-3.mp3").play();
+      break;
+    case "l":
+      new Audio("./sounds/tom-4.mp3").play();
+      break;
+
+    default:
+  }
+}
